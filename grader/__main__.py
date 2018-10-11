d1e66183cc3ce888848c5da91120d189a6341a5f
Fedor Korotkiy <prime@yandex-team.ru>
2018-10-11 19:10:25 +0300

    Add os url

--- grader/__main__.py
+++ grader/__main__.py
@@ -16,7 +16,12 @@ import pathlib
 from .task import Task
 
 
-def push_report(user_id, task, failed=False):
+def push_report(user_id, task, course_name, failed=False):
+    if course_name.startswith("cpp"):
+        url = "https://cpp.manytask.org/api/report"
+    else:
+        url = "https://os.manytask.org/api/report"
+
     # Do not expose token in logs.
     for _ in range(3):
         data = {
@@ -51,9 +56,9 @@ def grade():
         if task.review:
             return
 
-        push_report(user_id, task_name)
+        push_report(user_id, task_name, course_name)
     except subprocess.CalledProcessError:
-        push_report(user_id, task_name, failed=True)
+        push_report(user_id, task_name, course_name, failed=True)
         raise
 
 
