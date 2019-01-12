0028628ae412e9f3e810ff7f60e9ee391210fcdf
Veronika Ivannikova <veivannikova@avito.ru>
2019-01-12 10:02:00 +0300

    disable private test for a while

--- grader/hse.py
+++ grader/hse.py
@@ -28,20 +28,20 @@ class HsePyTask(task.Task):
         print('--- public tests passed ---')
 
         print('--- private tests started ---')
-        try:
-            self.check_call(
-                ["py.test", "../" + str(self.task_private_path), ],
-                env=dict(os.environ, PYTHONPATH="."),
-                cwd=str(self.task_path),
-                sandboxed=False,
-                timeout=60,
-                stdout=subprocess.DEVNULL,
-                stderr=subprocess.DEVNULL,
-            )
-        except subprocess.CalledProcessError:
-            print('Private tests failed. Something wrong in your code. Try harder.')
-            print('---')
-            exit(1)
+        # try:
+        #     self.check_call(
+        #         ["py.test", "../" + str(self.task_private_path), ],
+        #         env=dict(os.environ, PYTHONPATH="."),
+        #         cwd=str(self.task_path),
+        #         sandboxed=False,
+        #         timeout=60,
+        #         stdout=subprocess.DEVNULL,
+        #         stderr=subprocess.DEVNULL,
+        #     )
+        # except subprocess.CalledProcessError:
+        #     print('Private tests failed. Something wrong in your code. Try harder.')
+        #     print('---')
+        #     exit(1)
         print('--- private tests passed ---')
 
     def check_author_solution(self, submit_root):
