0defa4eae4041fc483e76b54903b4f3cddf10c25
Nadezhda Savchenko <nadynady98@mail.ru>
2020-11-07 16:31:06 +0300

    fix

--- grader/ema.py
+++ grader/ema.py
@@ -14,10 +14,4 @@ class EmaTask(task.Task):
                         cwd=str(self.root / self.name),
                         timeout=10800)
 
-        shutil.copytree("/tmp/artifacts_1", str(pathlib.Path(submit_root) / "artifacts_1"))
-        shutil.copytree("/tmp/artifacts_2", str(pathlib.Path(submit_root) / "artifacts_2"))
-        shutil.copytree("/tmp/artifacts_3", str(pathlib.Path(submit_root) / "artifacts_3"))
-        shutil.copytree("/tmp/artifacts_4", str(pathlib.Path(submit_root) / "artifacts_4"))
-        shutil.copytree("/tmp/artifacts_5", str(pathlib.Path(submit_root) / "artifacts_5"))
-        shutil.copytree("/tmp/artifacts_6", str(pathlib.Path(submit_root) / "artifacts_6"))
-        shutil.copytree("/tmp/artifacts_7", str(pathlib.Path(submit_root) / "artifacts_7"))
+        shutil.copytree("/tmp/artifacts", str(pathlib.Path(submit_root) / "artifacts"))
